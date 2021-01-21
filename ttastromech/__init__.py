c95ddc453866e840587d331f1b2f2fa8e3f96b3c
walchko <walchko@users.noreply.github.com>
2021-01-20 20:46:21 -0500

    removed build_utils

--- ttastromech/__init__.py
+++ ttastromech/__init__.py
@@ -1,6 +1,11 @@
 from .ttastromech import TTAstromech
 from .ttastromech_pyaudio import TTAstromechPyAudio
-from .version import __version__
+
+try:
+    from importlib.metadata import version # type: ignore
+except ImportError:
+    from importlib_metadata import version # type: ignore
 
 __author__ = 'Kevin J. Walchko'
 __license__ = 'MIT'
+__version__ = version("ttastromech")
