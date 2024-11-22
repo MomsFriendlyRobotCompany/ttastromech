6fbc7ecc5bd08a6b0da1c2544d7eb33f4eb3c23f
jrwarwick <justin.warwick@gmail.com>
2024-11-22 15:19:43 -0800

    Simple test case to exercise double letter cases.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,3 +9,8 @@ def test_lowercase_letters_only():
    r2.speak("hello world")
    assert True
 
+def test_double_letters():
+   r2 = TTAstromech()
+   r2.speak("g a giggle")
+   r2.speak("u o u o uu oo")
+   assert True
